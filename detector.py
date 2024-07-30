c8543e45380cb97c659e2862c0d2afb9469f6d28
Muzi Zwane <87149575+MuziZwane@users.noreply.github.com>
2024-07-30 07:37:09 +0000

    started working on the detector.py

---
+++ detector.py
@@ -0,0 +1,7 @@
+from path import Path
+
+default_encoding_path = Path("output/encoding.pkl")
+
+path("training").mkdir(exist_ok=True)
+path("output").mkdir(exist_ok=True)
+path("output").mkdir(exist_ok=True)
